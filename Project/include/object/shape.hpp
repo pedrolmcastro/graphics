d76e4bcfb51ca592046952750f5910eafc3976ee
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2023-04-19 15:40:29 -0300

    Add shape transform acessors

--- Project/include/object/shape.hpp
+++ Project/include/object/shape.hpp
@@ -9,8 +9,34 @@ namespace object {
     template<class Drawer> struct Shape final {
         Transform transform;
 
+
         auto draw(render::Program const& program) const -> void {
             Drawer::get(program).draw(transform);
+        }
+
+
+        [[nodiscard]] auto& translation() noexcept {
+            return transform.translation;
+        }
+
+        [[nodiscard]] auto& translation() const noexcept {
+            return transform.translation;
+        }
+
+        [[nodiscard]] auto& rotation() noexcept {
+            return transform.rotation;
+        }
+
+        [[nodiscard]] auto& rotation() const noexcept {
+            return transform.rotation;
+        }
+
+        [[nodiscard]] auto& scale() noexcept {
+            return transform.scale;
+        }
+
+        [[nodiscard]] auto& scale() const noexcept {
+            return transform.scale;
         }
     };
 }
