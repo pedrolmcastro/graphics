f8c8dcd7308eed4027ad875bfb9b139a1eef63ad
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2023-04-23 17:18:12 -0300

    Add group member comments to source files

--- Project/src/render/program.cpp
+++ Project/src/render/program.cpp
@@ -1,3 +1,12 @@
+/*
+    Álvaro José Lopes                  - 10873365
+    Natan Henrique Sanches             - 11795680
+    Gabriel da Cunha Dertoni           - 11795717
+    Pedro Lucas de Moliner de Castro   - 11795784
+    João Guilherme Jarochinski Marinho - 10698193
+*/
+
+
 #include <GL/glew.h>
 
 #include "math.hpp"
